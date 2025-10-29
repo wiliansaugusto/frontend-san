3a5840977a2780efce7508a8d6c68a0955a2b314
wiliansaugusto <wiliansaugusto@gmail.com>
2025-10-29 18:47:47 -0300

    Create navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './view/home/home.component';
+import { NotfoundComponent } from './view/notfound/notfound.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: '**', component: NotfoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
